0ef5dc67d92056ed457e7721fd358de39e66965f
Letícia <leticiaborchardt123@gmail.com>
2024-06-06 13:22:25 -0300

    criação do crud com postgres

--- CrudConexaoPostgres/src/main/java/com/company/DbFunction.java
+++ CrudConexaoPostgres/src/main/java/com/company/DbFunction.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DbFunction {
     public Connection connectToDb(String dbName, String user, String password) {
@@ -22,5 +24,103 @@ public class DbFunction {
         }
 
         return connection;
+    }
+
+    public void createTable(Connection connection, String tablename) {
+        Statement statement;
+
+        try {
+            String query = "CREATE TABLE IF NOT EXISTS " + tablename + "(id SERIAL, name VARCHAR(200), address VARCHAR(200), PRIMARY KEY (id))";
+
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Table '" + tablename + "' created!");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void insertRow(Connection connection, String tablename, String name, String address) {
+        Statement statement;
+
+        try {
+            String query = String.format("INSERT INTO %s (name, address) VALUES ('%s', '%s')", tablename, name, address);
+
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Row inserted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void readData(Connection connection, String tablename) {
+        Statement statement;
+
+        try {
+            String query = String.format("SELECT * FROM %s", tablename);
+
+            statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                System.out.print(result.getString("id") + " ");
+                System.out.print(result.getString("name") + " ");
+                System.out.println(result.getString("address") + " ");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void updateName(Connection connection, String tablename, Integer id, String newName) {
+        Statement statement;
+
+        try {
+            String query = String.format("UPDATE %s SET NAME = '%s' WHERE id = %s", tablename, newName, id);
+
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Update succeed");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void searchByName(Connection connection, String tablename, String name) {
+        Statement statement;
+
+        try {
+            String query = String.format("SELECT * FROM %s WHERE name LIKE '%s'", tablename, name);
+
+            statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                System.out.print(result.getString("id") + " ");
+                System.out.print(result.getString("name") + " ");
+                System.out.println(result.getString("address") + " ");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void delete(Connection connection, String tablename, Integer id) {
+        Statement statement;
+
+        try {
+            String query = String.format("DELETE FROM %s WHERE id = %s", tablename, id);
+
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Delete succeed");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- CrudConexaoPostgres/src/main/java/com/company/Main.java
+++ CrudConexaoPostgres/src/main/java/com/company/Main.java
@@ -1,9 +1,30 @@
 package com.company;
 
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         DbFunction db = new DbFunction();
 
-        db.connectToDb("testedb", "postgres", "password");
+        Connection connection = db.connectToDb("testedb", "postgres", "password");
+
+        System.out.println("Criação da tabela");
+        db.createTable(connection, "employee");
+
+        System.out.println("Inserindo registros");
+        db.insertRow(connection, "employee", "John", "Brasil");
+        db.insertRow(connection, "employee", "Bob", "Brasil");
+
+        System.out.println("Lendo registros");
+        db.readData(connection, "employee");
+
+        System.out.println("Atualizando registro");
+        db.updateName(connection, "employee", 1, "Jonas");
+
+        System.out.println("Pesquisando registros pelo nome");
+        db.searchByName(connection, "employee", "Fábio");
+
+        System.out.println("Removendo registro");
+        db.delete(connection, "employee", 2);
     }
 }
