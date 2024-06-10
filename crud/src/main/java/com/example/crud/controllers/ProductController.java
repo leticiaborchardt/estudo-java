5daa4e4746b77314253a43581c0a6f072c3532e2
Letícia <leticiaborchardt123@gmail.com>
2024-06-10 15:44:32 -0300

    tratamento de erros

--- crud/src/main/java/com/example/crud/controllers/ProductController.java
+++ crud/src/main/java/com/example/crud/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -47,7 +48,7 @@ public class ProductController {
             return ResponseEntity.ok(product);
         }
 
-        return ResponseEntity.notFound().build();
+        throw new EntityNotFoundException();
     }
 
     @DeleteMapping("/{id}")
@@ -63,6 +64,6 @@ public class ProductController {
             return ResponseEntity.noContent().build();
         }
 
-        return ResponseEntity.notFound().build();
+        throw new EntityNotFoundException();
     }
 }
